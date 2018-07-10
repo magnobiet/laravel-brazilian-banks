85a75de5d6271ecce2f847bc43b4cf3afdac4370
Magno Biét <magno.biet@gmail.com>
2018-07-10 19:36:59 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Bancos do Brasil (Laravel)
+
+Esta base foi criada com informações da [Federação Brasileira de Bancos](http://portal.febraban.org.br/).
+
+## Como usar?
+
+1. Baixe e copie as *migrations* e *seeds* para sua aplicação;
+2. Execute `php artisan migrate` e em seguida `php artisan db:seed`
+
+## Links úteis
+
+- Busca Banco (http://www.buscabanco.org.br/AgenciasBancos.asp)
+
+## Última atualização de dados
+
+03/01/2018
+
+## Licença
+
+Este projeto está licenciado sob a licença [MIT](http://magno.mit-license.org/2018). Copyright © Magno Biét
---
+++ app/Models/Bank.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bank extends Model
+{
+
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'website',
+        'compensation_code',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'deleted_at',
+    ];
+
+}
